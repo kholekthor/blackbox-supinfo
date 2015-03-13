d85d97a88e305ba8dbf2a5504e9bfd1e5f4569d7
Reda El Ghomari <redareda9@hotmail.fr>
2015-03-13 17:51:26 +0100

    Ebauche tableau des centres
    Tableau des centres pour le placement de l'atome

--- board.py
+++ board.py
@@ -1,31 +1,46 @@
 import pygame, sys
 from pygame.locals import *
 
-
-
-#initialisation du module pygame et mixer 
+#initialisation du module pygame
 pygame.init()
-
-
 #Création de la fenêtre
 fenetre = pygame.display.set_mode((1000, 700))
 background_color = (31, 31, 31)
 fenetre.fill(background_color)
 pygame.display.flip()
-
-
+#Chargement des images correspondants aux cases des rayons et des atomes
 square = pygame.image.load("img/square.jpg")
+ray_square = pygame.image.load("img/ray_square.jpg")
 
-def draw_lines(number):
-    for i in range(number):
-        fenetre.blit(square, (100 + i*61,50))
+class Atom(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+       pygame.sprite.Sprite.__init__(self)
 
-    for j in range(number):
-        for i in range(number + 2):
-            fenetre.blit(square, (39 + i*61,108 + j*58))
+       self.image = pygame.image.load("img/atom.png")
+       self.rect = self.image.get_rect()
 
-    for i in range(number):
-        fenetre.blit(square, (100 + i*61, 50 + 58*(number+1)))
+
+def init_centers_array(size):
+    #Initialise un tableau des centres qui contient les centres de toutes les cases du jeu
+    for i in range(size):
+        #130,195 premier centre
+        pass
+
+
+def draw_lines(size):
+    for i in range(size):
+        fenetre.blit(ray_square, (100 + i*61,50))
+
+    for i in range(size):
+        fenetre.blit(ray_square, (100 + i*61, 50 + 58*(size+1)))
+
+    for j in range(size):
+        for i in range(size + 2):
+            if i == 0 or i == size + 1:
+                fenetre.blit(ray_square, (39 + i*61,108 + j*58))
+            else:
+                fenetre.blit(square, (39 + i*61,108 + j*58))
+
 
 def draw_interface(difficulty_level):
     #Si le mode de diffidulté est facile
@@ -39,29 +54,25 @@ def draw_interface(difficulty_level):
     elif difficulty_level == "HARD":
         draw_lines(8)
 
-
-#déclaration des couleurs
-
-
 #variables 
 etatPartie = True 
 click = 0
 a, b, c, d, e, f = 0, 0, 0, 0, 0, 0
 
-draw_interface("EASY")
+draw_interface("HARD")
 
 #MAIN 
 while True:
-    for event in pygame.event.get(): #Evenement on quitte le jeu
+    for event in pygame.event.get():
+        #Evenement on quitte le jeu
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         
         #Evenement clic gauche souris choix position sphère  
-        elif event.type == MOUSEBUTTONDOWN and event.button == 1 and click < 3 and etatPartie == True:#condition d'entrée
+        elif event.type == MOUSEBUTTONDOWN and event.button == 1 and click < 3 and etatPartie:#condition d'entrée
                 x = event.pos[0]
                 y = event.pos[1]
-                pygame.draw.circle(fenetre,YELLOW, (x,y),12)
 
                 click += 1
                 
